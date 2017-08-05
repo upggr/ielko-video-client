7fb48b87b61dc1fad9a8707c33a341e674b37adf
Ioannis Kokkinis <info@upg.gr>
2017-08-05 21:05:57 +0300

    Updated ielko-wp-media-manager.php.

--- ielko-wp-media-manager.php
+++ ielko-wp-media-manager.php
@@ -603,9 +603,9 @@ function ivc_settings_section_intro(  ) {
   Your TVOS feed is accessible from <a href="'.get_site_url().'/?feed=tvos">'.get_site_url().'/?feed=tvos</a><br />
 	Your Android (Variant 1) feed is accessible from <a href="'.get_site_url().'/?feed=android1">'.get_site_url().'/?feed=android1</a><br />
 	<br />
-	<form action="/http://factory.upg.gr/" method="post">
+	<form action="http://factory.upg.gr/" method="post">
 		<input type="hidden" value="test" name="test" />
-  <button type="submit" formmethod="post" formaction="http://yahoo.com">Download Roku app</button>
+  <button type="submit" >Download Roku app</button>
 </form>
     ', 'wordpress' );
 
