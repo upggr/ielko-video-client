9562ba9da6ba78c233b2d9c1d5fa28af30633b32
Ioannis Kokkinis <info@upg.gr>
2017-08-06 15:29:23 +0300

    Updated ielko.js.

--- js/ielko.js
+++ js/ielko.js
@@ -40,6 +40,8 @@
           splash_screen_hd: $('#roku_app').attr("splash_screen_hd"),
           splash_screen_sd: $('#roku_app').attr("splash_screen_sd"),
           splash_screen_fhd: $('#roku_app').attr("splash_screen_fhd")
+          overhang_sd: $('#roku_app').attr("overhang_sd")
+          overhang_hd: $('#roku_app').attr("overhang_hd")
 
 
         },
