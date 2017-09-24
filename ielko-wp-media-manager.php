3408788bab793889760043aa46e4cfb76eaa5ff0
Ioannis Kokkinis <info@upg.gr>
2017-09-24 21:20:08 +0300

    Updated ielko-wp-media-manager.php.

--- ielko-wp-media-manager.php
+++ ielko-wp-media-manager.php
@@ -536,8 +536,8 @@ if ($thecategory == $_GET['cat']) {
 						$thePosts = query_posts("cat=$thecatid&posts_per_page=100&post_type=media_item");
 						global $wp_query;
 						$noposts= $wp_query->found_posts;
-						echo '<resultLength>'.$noposts.'</resultLength>';
-						echo '<endIndex>'.$noposts.'</endIndex>';
+						echo '<resultLength>3</resultLength>';
+						echo '<endIndex>3</endIndex>';
 					while (have_posts()) : the_post();
           $thetitle = get_the_title();
           $theurl = get_post_meta(get_the_ID(), 'media_url', true);
