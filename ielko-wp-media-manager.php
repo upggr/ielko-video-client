5340d90cd183be2f5f579186d83e169eeac12fca
Ioannis Kokkinis <info@upg.gr>
2017-09-26 00:38:42 +0300

    Updated ielko-wp-media-manager.php.

--- ielko-wp-media-manager.php
+++ ielko-wp-media-manager.php
@@ -788,6 +788,7 @@ function checkurl($url) {
 	//$result = '';
    echo exec('/usr/bin/ffprobe -show_format '.$url, $output, $result);
 	// return $output;
+	ini_set('default_socket_timeout', 2);
 	$headers = @get_headers( $url);
 	$headers = (is_array($headers)) ? implode( "\n ", $headers) : $headers;
 	return (bool)preg_match('#^HTTP/.*\s+[(200|301|302)]+\s#i', $headers);
